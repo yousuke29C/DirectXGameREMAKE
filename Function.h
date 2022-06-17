cc41b6d382644adf7b75613855c74579bd03cd5d
LC1C_29 <k021g1384@g.neec.ac.jp>
2022-06-17 15:35:11 +0900

    re_02_05

---
+++ Function.h
@@ -0,0 +1,45 @@
+#include "WorldTransform.h"
+
+/// <summary>
+/// 単位行列作成
+/// </summary>
+/// <returns>単位行列</returns>
+Matrix4 CreateMatIdentity();
+
+/// <summary>
+/// スケーリング行列の作成
+/// </summary>
+/// <param name="scale">WorldTransformのscale_</param>
+/// <returns>作成したスケーリング行列</returns>
+Matrix4 CreateMatScale(Vector3 scale);
+
+/// <summary>
+/// Z軸の回転行列作成
+/// </summary>
+/// <param name="rotation">WorldTransformのrotation_</param>
+/// <returns>Z軸周りの回転行列</returns>
+Matrix4 CreateMatRotationZ(Vector3 rotation);
+
+/// <summary>
+/// X軸の回転行列作成
+/// </summary>
+/// <param name="rotation">WorldTransformのrotation_</param>
+/// <returns>X軸の回転行列</returns>
+Matrix4 CreateMatRotationX(Vector3 rotation);
+
+
+
+/// <summary>
+/// Y軸の回転行列
+/// </summary>
+/// <param name="rotation">WorldTransformのrotation_</param>
+/// <returns>Y軸の回転行列</returns>
+Matrix4 CreateMatRotationY(Vector3 rotation);
+
+
+/// <summary>
+/// 平行移動
+/// </summary>
+/// <param name="translation">WorldTransformのTranslation_</param>
+/// <returns>平行移動</returns>
+Matrix4 CreateMatTranslation(Vector3 translation);
--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -2,6 +2,7 @@
 #include "TextureManager.h"
 #include <cassert>
 #include "PrimitiveDrawer.h"
+#include "Function.h"
 #include <random>
 #define PI 3.14
 
@@ -26,96 +27,7 @@ std::mt19937_64 engine(seed_gen());
 std::uniform_real_distribution<float> posDist(-10.0f, 10.0f);
 std::uniform_real_distribution<float> rotDist(0.0f, 3.14f);
 
-/// <summary>
-/// 単位行列作成
-/// </summary>
-/// <returns>単位行列</returns>
-Matrix4 CreateMatIdentity() {
-	Matrix4 mat;
-	mat.m[0][0] = 1;
-	mat.m[1][1] = 1;
-	mat.m[2][2] = 1;
-	mat.m[3][3] = 1;
-	return mat;
-}
 
-/// <summary>
-/// スケーリング行列の作成
-/// </summary>
-/// <param name="scale">WorldTransformのscale_</param>
-/// <returns>作成したスケーリング行列</returns>
-Matrix4 CreateMatScale(Vector3 scale) {
-	Matrix4 mat;
-	mat.m[0][0] = scale.x;
-	mat.m[1][1] = scale.y;
-	mat.m[2][2] = scale.z;
-	mat.m[3][3] = 1.0f;
-
-	return mat;
-}
-
-/// <summary>
-/// Z軸の回転行列作成
-/// </summary>
-/// <param name="rotation">WorldTransformのrotation_</param>
-/// <returns>Z軸周りの回転行列</returns>
-Matrix4 CreateMatRotationZ(Vector3 rotation) {
-	Matrix4 mat = CreateMatIdentity();
-	mat.m[0][0] = cos(rotation.z);
-	mat.m[0][1] = sin(rotation.z);
-	mat.m[1][0] = -sin(rotation.z);
-	mat.m[1][1] = cos(rotation.z);
-
-	return mat;
-}
-
-/// <summary>
-/// X軸の回転行列作成
-/// </summary>
-/// <param name="rotation">WorldTransformのrotation_</param>
-/// <returns>X軸の回転行列</returns>
-Matrix4 CreateMatRotationX(Vector3 rotation) {
-	Matrix4 mat = CreateMatIdentity();
-	mat.m[1][1] = cos(rotation.x);
-	mat.m[1][2] = sin(rotation.x);
-	mat.m[2][1] = -sin(rotation.x);
-	mat.m[2][2] = cos(rotation.x);
-
-	return mat;
-}
-
-
-
-/// <summary>
-/// Y軸の回転行列
-/// </summary>
-/// <param name="rotation">WorldTransformのrotation_</param>
-/// <returns>Y軸の回転行列</returns>
-Matrix4 CreateMatRotationY(Vector3 rotation) {
-	Matrix4 mat = CreateMatIdentity();
-	mat.m[0][0] = cos(rotation.y);
-	mat.m[0][2] = -sin(rotation.y);
-	mat.m[2][0] = sin(rotation.y);
-	mat.m[2][2] = cos(rotation.y);
-
-	return mat;
-}
-
-
-/// <summary>
-/// 平行移動
-/// </summary>
-/// <param name="translation">WorldTransformのTranslation_</param>
-/// <returns>平行移動</returns>
-Matrix4 CreateMatTranslation(Vector3 translation) {
-	Matrix4 lat = CreateMatIdentity();
-	lat.m[3][0] = translation.x;
-	lat.m[3][1] = translation.y;
-	lat.m[3][2] = translation.z;
-	lat.m[3][3] = 1;
-
-	return lat;
-}
 
 //Matrix4 CreateMatRot(Vector3 Rotation) {
 //	Matrix4 Rot = CreateMatIdentity();
